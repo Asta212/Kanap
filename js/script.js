5c00351348e4e4ad2134f57714a07d2b5ffc952b
Amine Laaboudi <laaboudi.amine212@gmail.com>
2022-07-19 16:14:41 +0200

    test

--- js/script.js
+++ js/script.js
@@ -1,15 +1,18 @@
 fetch("http://localhost:3000/api/products")
 .then((res) => res.json())
-.then((data) => {
+.then((data) => addproducts(data))
+
+function addproducts(data) {
     console.log(data)
     const imageUrl = data[0].imageUrl
     console.log("url de l'image", imageUrl)
     const anchor = document.createElement("a")
     anchor.href = "http://localhost:3000/images/kanap01.jpeg"
+    anchor.text = "super canap"
     
     const items = document.querySelector("#items")
     if (items != null) {
         items.appendChild(anchor)
         console.log("nous avons bien ajouté le lien")
     }
-})
+}
